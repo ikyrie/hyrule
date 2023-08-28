bd906658ba7afd185101836b1bee15fb704a8b80
ikyrie <mah.c5r@gmail.com>
2023-08-28 16:55:23 -0300

    Creating Utils

--- lib/data/api/data_api.dart
+++ lib/data/api/data_api.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:hyrule/domain/business/api_workflow.dart';
 import 'package:hyrule/domain/models/entry.dart';
+import 'package:hyrule/utils/consts/api.dart';
 
 class DataApi implements ApiWorkflow {
   final Dio dio = Dio();
@@ -8,8 +9,7 @@ class DataApi implements ApiWorkflow {
 
   @override
   Future<List<Entry>> getEntriesByCategory ({required String category}) async {
-    String url = "https://botw-compendium.herokuapp.com/api/v3/compendium/category/$category";
-    final response = await dio.get(url);
+    final response = await dio.get(url + category);
     list = List<Entry>.from(response.data["data"].map((e) => Entry.fromMap(e)));
     return list;
   }
