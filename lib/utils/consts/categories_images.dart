1382c2ee46ab5390b26e96f81943ac9744aa804c
jhoisz <jvrda@ufpi.edu.br>
2023-08-29 13:50:02 -0300

    Added categories images

---
+++ lib/utils/consts/categories_images.dart
@@ -0,0 +1,7 @@
+List<String> categoriesImages = [
+  'assets/images/creature.png',
+  'assets/images/equipment.png',
+  'assets/images/material.png',
+  'assets/images/monsters.png',
+  'assets/images/treasure.png',
+];
