85e7dac093dda4ad3d51d49343ebbadb2009446d
jhoisz <jvrda@ufpi.edu.br>
2023-08-29 13:52:33 -0300

    Created details entry screen

---
+++ lib/presenter/screens/details.dart
@@ -0,0 +1,71 @@
+
+import 'package:flutter/material.dart';
+
+import '../../domain/models/entry.dart';
+
+class Details extends StatelessWidget {
+  const Details({super.key, required this.entry});
+  final Entry entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Detalhes"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.bookmark),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Text(entry.name),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Row(
+                    children: [
+                      Chip(label: Text(entry.category)),
+                      if (entry.dlc)
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: Chip(label: Text('DLC')),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: Image(
+                      image: NetworkImage(entry.image),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Text(entry.description),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
