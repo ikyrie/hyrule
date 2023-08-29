de595cdf92630814c0cdf1b3681c2d42c56076b7
jhoisz <jvrda@ufpi.edu.br>
2023-08-29 14:43:43 -0300

    Added dismissible and changed card shape

--- lib/presenter/components/entry_card.dart
+++ lib/presenter/components/entry_card.dart
@@ -13,72 +13,96 @@ class EntryCard extends StatelessWidget {
     return Card(
       clipBehavior: Clip.hardEdge,
       margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-      child: SizedBox(
-        height: 180,
+      child: Dismissible(
+        direction: isSaved ? DismissDirection.endToStart : DismissDirection.none,
+        key: ValueKey<int>(entry.id),
+        background: Container(
+          color: Colors.redAccent,
+          child: const Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Icon(Icons.delete),
+            ),
+          ),
+        ),
+        onDismissed: (direction) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('deletado')));
+        },
         child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => Details(
-                  entry: entry,
-                ),
-              ),
-            );
-          },
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image(
-                    image: NetworkImage(entry.image),
-                    fit: BoxFit.cover,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => Details(
+                    entry: entry,
                   ),
                 ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              );
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 180,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(entry.name),
-                          Text(entry.description),
-                        ],
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image(
+                            image: NetworkImage(entry.image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      Wrap(
-                        children: [
-                          Chip(label: Text(entry.category)),
-                          if (entry.dlc)
-                            const Padding(
-                              padding: EdgeInsets.only(left: 8.0),
-                              child: Chip(label: Text('DLC') ),
-                            ),
-                          // TODO: fix delete button
-                          // if (isSaved)
-                          //   IconButton(
-                          //     padding: EdgeInsets.zero,
-                          //     onPressed: () {},
-                          //     icon: const Icon(Icons.delete),
-                          //   )
-                        ],
+                      Flexible(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(entry.name),
+                                  Text(entry.description),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       )
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: Wrap(
+                    spacing: 8.0,
+                    children: [
+                      Chip(label: Text(entry.category)),
+                      Chip(label: Text(entry.category)),
+                      Chip(label: Text(entry.category)),
+                      Chip(label: Text(entry.category)),
+                      Chip(label: Text(entry.category)),
+
+                      if (entry.dlc)
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: Chip(label: Text('DLC')),
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
